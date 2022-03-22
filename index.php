1905740fac11da730ca36b77de833750e562e2fd
tek707 <tekkwaku7@gmail.com>
2022-03-22 01:14:18 +0000

    main-re-commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 
-
+<?php  include_once 'conn.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,81 +11,87 @@
 </head>
 <body>
 <?php
+$result = $mysqli->query("SELECT * FROM list ORDER BY date ASC" )or die ($mysqli->error);
 ?>
   <div class="container">
 
     <div class="heading">
-Group Chat
+ To-Do-List
  <hr>
     </div>
+    <?php
+    while ($row =$result->fetch_assoc()): ?>
    
-<div class="scroll">
-
-<!-- left message -->
-    <div class="left mainpage"> 
-<div class="left_msg">
-  <div class="left_msg_head">
-<img class="left_msg_avater" src="avatar.png" alt="avater"/>
-                <h3 class="msg_left_username">lomi</h3>
-                </div> 
-   <!--left main content -->
-    <p class="msg_left_text">this is other people's text</p> 
-<p class="msg_left_date">  10:14pm </p> 
-  </div>
-    </div>
-    
-<!-- end of left message -->
-
-<br>
-
-<!-- start of right message -->
-
-    <div class="right mainpage">
-    <div class="right_msg">
-    <div class="right_msg_head">
-<img class="right_msg_avater" src="avatar.png" alt="avater"/>
-                <h3 class="msg_right_username">Titiati</h3>
-                </div> 
-   <!--right main content -->
-    <h3 class="msg_right_text">this is your text</h3> 
-<p class="msg_right_date">  10:15pm</p> 
-
-  </div>
-    </div>
-
-<!-- end of right message -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <!-- div for input box and send btn -->
+  
+   
 
   
+
+
+
+
+    <div class="mainpage">
+<div class="lstcontent">
+  <!-- div for edit and delete -->
+  <div class="editlist">
+  <a href="?edit=<?php echo $row['list_id']; ?>">  <button name="Update" class="cmdicons"> <img src="edit.png" alt="edit"></button></a>
+   
+  
+  
+  <a href="conn.php?delete=<?php echo $row['list_id']; ?>"><button name="delete" class="cmdicons"><img src="delete.png" alt="delete"></button></a>
+    </div>
+    <br>
+   <!-- main content -->
+    <p class="typo"><?php echo $row['item']; ?>   </p> 
+<br>
+<p class="date"> <?php echo $row['date']; ?>  </p> 
+
+  </div>
+
+    </div>
+    <?php endwhile; ?>
+    <!-- div for input box and send btn -->
+
+    <?php 
+        //edit button php function
+        if(isset($_GET['edit'])){
+          $list_id = $_GET['edit'];
+         
+            $update = true;
+             $Res=$mysqli->query("SELECT * FROM `list` WHERE `list_id`=$list_id") or die($mysqli->error);
+           
+             if ($Res->num_rows == 1) {
+              $r= mysqli_fetch_array($Res);
+             
+               $item = $r['item'];
+            }
+        
+          }else{
+          
+            $item = "";
+          }
+        
+        ?>
       
-      </div>
+
 
     <div class="listcreater">
       <form action="conn.php" method="post">
       <input type="hidden"  name="list_id" value="<?php echo $list_id ; ?>">
 
-      <input type="text" class="listcont" name="item"  required>
+      <input type="text" class="listcont" name="item" value="<?php echo $item ; ?>" required>
 
 
-     
+      <?php 
+if ($update == true):
+?>
+  <button type="submit" name="Update" class="send" value="Update" id="Update"><img src="send.png" alt="submit"></button>
+
+<?php else :?>
   <button type="submit" name="send" class="send" value="send" id="send"><img src="send.png" alt="submit"></button>
 
 
+  <?php  endif; ?>
 </form>
     </div>
   </div>  
